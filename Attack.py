e8c21dff59fac3570ba8717babf7bf114d4d24bb
sadari1 <sadari@ufl.edu>
2019-12-23 10:28:37 -0500

    Attack.py contains the code for the attack but in a modularized fashion so that it can work with any attack model. S2VT_Attack.py contains the specific details needed to carry out the attack; Attack.py simply calls on the corresponding variables and methods to run the attack. S2VT_Attack_Launcher.py is the file to run to launch the attack using the S2VT model. It uses the sliding window technique.

---
+++ Attack.py
@@ -0,0 +1,197 @@
+import os
+import numpy as np
+import torch
+import logging
+import skvideo.io
+import matplotlib.pyplot as plt
+from video_caption_pytorch.misc import utils as utils
+from torch.autograd import Variable
+from torchvision import transforms
+from utils import *
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+# Change logging level to info if running experiment, debug otherwise
+logger.setLevel(logging.DEBUG)
+
+
+'''
+pass in an object (instance of a class):
+
+Ex: S2VT_Attack:
+learning rate
+num it
+batch size
+oracle 
+dataset
+phrase length is target
+vocab
+
+optimizer
+crit
+input_shape
+
+target
+real target
+tlabel, tmask
+
+loss function
+
+create batches
+
+'''
+
+#attack package is the attack object for whatever is passed in.
+#It is called oracle because it holds the information for basically everything that's needed
+class Attack:
+    def __init__(self, attack_package):
+        """
+        :param oracle: ImageCaptioner class
+        :param image: sample image to get shape
+        :param target: target caption (for now,
+        in future target can also be the image we want to extract target caption from)
+        """
+
+        self.oracle = attack_package
+    '''
+    video path, windows and such will stay.
+    
+    create batches will be part of the class function that you pass in. Attack algorithm otherwise remains the same.
+    
+    '''
+
+    '''
+    The attack: original component, delta (mask), and decode/test phase to calculate gradients
+    
+    
+    '''
+    # Execute uses the image path directly. Fix out_dir later, for now it's the same directory (add an argument for output dir for argparse)
+    def execute(self, functional=False, stats=False):
+
+
+        c = self.oracle.c
+
+
+        # plt.imshow(frames[0])
+        # plt.show()
+
+        #
+        # original = torch.tensor(self.oracle.frames)
+        # original = (original.float()).cuda()
+
+        original = self.oracle.original()
+
+        #Original caption/classification, make this a function as well
+        # with torch.no_grad():
+        #     # bp ---
+        #     batch = self.oracle.create_batches(frames_to_do=original)
+        #     seq_prob, seq_preds = self.oracle.model(batch.unsqueeze(0), mode='inference')
+        #     sents = utils.decode_sequence(self.oracle.vocab, seq_preds)
+        #
+        #     for sent in sents:
+        #         print('Original caption: ' + sent)
+
+
+        # all the frames are stored in batches. Batches[0] should contain the first 32 frames.
+        torch.cuda.empty_cache()
+
+        # The attack
+        for i in range(self.oracle.num_iterations):
+
+
+            print("Video path: ", self.oracle.video_path)
+
+            #Make this a part of the attack model.
+
+
+
+            cost, decoded, apply_delta, pass_in = self.oracle.costs_decoded(original)
+            logger.info("Decoding at iteration {}: {} ".format(i, decoded))
+
+            #Cost, decoded = self.oracle(
+            #Instead of sents, maybe it's decoded (so action label becomes target label for example)
+            if decoded == self.oracle.real_target or i == (self.oracle.num_iterations - 1):
+
+                # We're done
+                logger.debug("Decoding at iteration {}:\t{} ".format(i, decoded))
+                logger.debug("Early stop. Cost: {}".format(cost))
+
+                base_toks = self.oracle.video_path.split('/')
+                base_dir_toks = base_toks[:-1]
+                base_filename = base_toks[-1]
+                base_name = ''.join(base_filename.split('.')[:-1])
+                adv_path = os.path.join('/'.join(base_dir_toks), base_name + '_adversarial.avi')
+
+                # plt_tensor(pass_in/255.)
+                if not functional:
+                    self.oracle.save_to(adv_path, apply_delta, pass_in)
+
+                if stats:
+                    return {'pass_in': pass_in.detach().cpu().numpy(),
+                            'iterations': i,
+                            'delta': self.oracle.delta.detach().cpu().numpy()}
+                else:
+                    return pass_in
+
+            # Every 10 iterations it outputs the caption.
+            if i % 1000 == 0:
+                # See how we're doing
+                logger.info("Decoding at iteration {}: {} ".format(i, decoded))
+                if not functional:
+                    self.oracle.plt_collate_batch(pass_in / 255.)
+
+            # if i % 20 == 0:
+            #     plt_tensor(pass_in/255.)
+
+            # print("Norm:\t{}\t\tCost:\t{}".format(apply_delta.norm(), cost.data))
+
+
+            #Since pass_in and original are already acquired based on the attack model,
+            #This can be left as is.
+
+            # w and y make calculations more efficient and are used to calculate the l2 norm
+            y = torch_arctanh(original / 255.).cuda()
+            w = torch_arctanh(pass_in / 255.) - y
+            normterm = ((w+y).tanh() - y.tanh())
+            normterm = normterm.mean(0).norm()
+            print("Cost:\t{}\t+\tNormterm:\t{}".format(cost, normterm))
+            cost = cost + (c * normterm)
+
+            # calculate gradients
+            self.oracle.optimizer.zero_grad()
+            cost.backward()
+            self.oracle.optimizer.step()
+
+            # Iteration and cost displayed at every step. We apply the perturbation to the original image again to find the adversarial caption.
+            logger.debug("\nIteration: {}, cost: {}".format(i, cost))
+            # torch.cuda.empty_cache()
+            # Every iteration it checks for whether or not the target caption equals the original
+
+#
+# def PIL_to_image(image_path):
+#     tensorize = transforms.ToTensor()
+#     original_pil = Image.open(image_path)
+#     image_tensor = tensorize(original_pil).unsqueeze(0)
+#     image_tensor = image_tensor.to(device)
+#     image_tensor = Variable(image_tensor)
+#     return image_tensor
+
+
+def torch_arctanh(x, eps=1e-6):
+    x *= (1. - eps)
+    return (torch.log((1 + x) / (1 - x))) * 0.5
+
+#
+# def plt_tensor(batched_t):
+#     showing = batched_t[0]
+#     if batched_t.shape[-1] == 3:
+#         showing = showing.detach().cpu().numpy()
+#     else:
+#         showing = showing.permute(1, 2, 0).detach().cpu().numpy()
+#
+#     plt.imshow(showing)
+#     plt.show()
+
+
---
+++ Attack_Models/S2VT_Attack.py
@@ -0,0 +1,253 @@
+import math
+import torch
+import torch.optim as optim
+import logging
+import skvideo.io
+from torch.autograd import Variable
+from torchvision import transforms
+from global_constants import *
+import matplotlib.pyplot as plt
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+import numpy as np
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+# Change logging level to info if running experiment, debug otherwise
+logger.setLevel(logging.DEBUG)
+
+
+class S2VT_Attack:
+    def __init__(self, model, video_path, target, dataset, config, optimizer, crit, seq_decoder, window=None ):
+        # self.seed = config.seed
+
+        self.video_path = video_path
+        self.batch_size = config["batch_size"]
+        if window:
+            self.frames = skvideo.io.vread(video_path)[window[0]:window[-1] + 1]
+        else:
+            self.frames = skvideo.io.vread(video_path)[0:self.batch_size]
+
+        # self.loss = loss
+
+        self.seq_decoder = seq_decoder
+        # dc = 0.80
+        self.c = config["c"]
+        self.dc = 255
+
+        self.num_iterations = config["num_iterations"]
+        self.phrase_length = len(target)
+        self.model = model
+        self.dataset = dataset
+        self.vocab = dataset.get_vocab()
+
+        if torch.cuda.is_available():
+            self.delta = Variable(torch.zeros(self.frames.shape).cuda(), requires_grad=True)
+        else:
+            self.delta = Variable(torch.zeros(self.frames.shape), requires_grad=True)
+
+        #optimizer is a list. Optimizer[0] is the function name, optimizer[1] is the parameters.
+        if optimizer[0] == 'Adam':
+            self.optimizer = optim.Adam([self.delta], lr=config["learning_rate"], betas=optimizer[1])
+
+        self.crit = crit
+
+        self.input_shape = config["input_shape"]
+        self.target = target
+        self.real_target = ' '.join(target.split(' ')[1:-1])
+
+        self.tlabel, self.tmask = self.produce_t_mask()
+
+    def loss(self, seq_prob):
+        loss = self.crit(seq_prob, self.tlabel[:, 1:].cuda(), self.tmask[:, 1:].cuda())
+        return loss
+
+    def produce_t_mask(self):
+        mask = torch.zeros(self.dataset.max_len)
+        captions = [self.target.split(' ')]
+        gts = torch.zeros(len(captions), self.dataset.max_len).long()
+        for i, cap in enumerate(captions):
+            if len(cap) > self.dataset.max_len:
+                cap = cap[:self.dataset.max_len]
+                cap[-1] = '<eos>'
+            for j, w in enumerate(cap):
+                gts[i, j] = self.dataset.word_to_ix[w]
+
+        label = gts[0]
+        non_zero = (label == 0).nonzero()
+        mask[:int(non_zero[0]) + 1] = 1
+
+        return label.unsqueeze(0), mask.unsqueeze(0)
+
+
+    def original(self):
+
+        print("Frame length: {}".format(len(self.frames)))
+
+        original = torch.tensor(self.frames)
+        original = (original.float()).cuda()
+
+        with torch.no_grad():
+            # bp ---
+            batch = self.create_batches(frames_to_do=original)
+            seq_prob, seq_preds = self.model(batch.unsqueeze(0), mode='inference')
+            sents = self.seq_decoder(self.vocab, seq_preds)
+
+            for sent in sents:
+                print('Original caption: ' + sent)
+
+        print('Target caption: ' + self.target)
+        return original
+
+
+
+
+    def costs_decoded(self, original):
+        apply_delta = torch.clamp(self.delta * 255., min=-self.dc, max=self.dc)
+
+        # This too
+        # The perturbation is applied to the original and resized through interpolation
+        pass_in = torch.clamp(apply_delta + original, min=0, max=255)
+
+        # Put this in the function as well to decode at each iteration
+        batch = self.create_batches(pass_in)
+        feats = self.model.conv_forward(batch.unsqueeze(0))
+        seq_prob, seq_preds = self.model.encoder_decoder_forward(feats, mode='inference')
+        cost = self.loss(seq_prob=seq_prob)
+        sents = self.seq_decoder(self.vocab, seq_preds)
+
+        return cost, sents[0], apply_delta, pass_in
+
+    def save_to(self, adv_path, apply_delta, pass_in):
+        def save_tensor_to_video(batched_t, fpath):
+            in_frames = batched_t.detach().cpu().numpy()
+            skvideo.io.vwrite(fpath, in_frames)
+
+        def plt_collate_batch(batched_t):
+            n_col = 2
+            n_rows = np.max([2, int(len(batched_t) / n_col)])
+            fig, axes = plt.subplots(nrows=n_rows, ncols=n_col, sharey=True, sharex=True)
+
+            k = 0
+            for i in range(n_rows):
+                for j in range(n_col):
+                    if (k >= len(batched_t)):
+                        showing = torch.from_numpy(np.ones(batched_t[0].shape))
+                    else:
+                        showing = batched_t[k]
+
+                    if batched_t.shape[-1] == 3:
+                        showing = showing.detach().cpu().numpy()
+                    else:
+                        showing = showing.permute(1, 2, 0).detach().cpu().numpy()
+
+                    axes[i, j].imshow(showing)
+                    k += 1
+
+            plt.tight_layout()
+            plt.show()
+
+
+        plt_collate_batch(pass_in / 255.)
+        logger.info("Saving adversarial video to:\t{}".format(adv_path))
+        save_tensor_to_video(pass_in, adv_path)
+        save_tensor_to_video(apply_delta, 'perturbation_' + adv_path)
+
+
+    def plt_collate_batch(batched_t):
+        n_col = 2
+        n_rows = np.max([2, int(len(batched_t) / n_col)])
+        fig, axes = plt.subplots(nrows=n_rows, ncols=n_col, sharey=True, sharex=True)
+
+        k = 0
+        for i in range(n_rows):
+            for j in range(n_col):
+                if (k >= len(batched_t)):
+                    showing = torch.from_numpy(np.ones(batched_t[0].shape))
+                else:
+                    showing = batched_t[k]
+
+                if batched_t.shape[-1] == 3:
+                    showing = showing.detach().cpu().numpy()
+                else:
+                    showing = showing.permute(1, 2, 0).detach().cpu().numpy()
+
+                axes[i, j].imshow(showing)
+                k += 1
+
+        plt.tight_layout()
+        plt.show()
+
+    def create_batches(self, frames_to_do):
+        batch_size = self.batch_size
+        class ToSpaceBGR(object):
+
+            def __init__(self, is_bgr):
+                self.is_bgr = is_bgr
+
+            def __call__(self, tensor):
+                if self.is_bgr:
+                    new_tensor = tensor.clone()
+                    new_tensor[0] = tensor[2]
+                    new_tensor[2] = tensor[0]
+                    tensor = new_tensor
+                return tensor
+
+        class ToRange255(object):
+
+            def __init__(self, is_255):
+                self.is_255 = is_255
+
+            def __call__(self, tensor):
+                if self.is_255:
+                    tensor.mul_(255)
+                return tensor
+
+        n = frames_to_do.shape[0]
+        h, w = frames_to_do.shape[1:3]
+        scale = 0.875
+        input_size = [3, 331, 331]
+        mean, std = [0.5, 0.5, 0.5], [0.5, 0.5, 0.5]
+        input_range = [0, 1.]
+        input_space = 'RGB'
+        expand_size = int(math.floor(max(input_size) / scale))
+        if w < h:
+            ow = expand_size
+            oh = int(expand_size * h / w)
+        else:
+            oh = expand_size
+            ow = int(expand_size * w / h)
+
+        tfs = []
+        tfs.append(ToSpaceBGR(input_space == 'BGR'))
+        tfs.append(ToRange255(max(input_range) == 255))
+        tfs.append(transforms.Normalize(mean=mean, std=std))
+        tf = transforms.Compose(tfs)
+
+        a = int((0.5 * oh) - (0.5 * float(input_size[1])))
+        b = a + input_size[1]
+        c = int((0.5 * ow) - (0.5 * float(input_size[2])))
+        d = c + input_size[2]
+
+        if n < batch_size:
+            logger.warning("Sample size less than batch size: Cutting batch size.")
+            batch_size = n
+
+        logger.info("Generating {} batches...".format(n // batch_size))
+
+        for idx in range(0, n, batch_size):
+            frames_idx = list(range(idx, min(idx + batch_size, n)))
+
+            # <batch, h, w, ch> <0,255>
+            batch_tensor = frames_to_do[frames_idx]
+
+            pass_in = batch_tensor.permute(0, 3, 1, 2) / 255.
+            inp = torch.nn.functional.interpolate(pass_in,
+                                                  size=(oh, ow),
+                                                  mode='bilinear', align_corners=True)
+            # Center cropping
+            cropped_frames = inp[:, :, a:b, c:d]
+            # cropped_image = cropped_image.contiguous()
+            for i in range(len(cropped_frames)):
+                cropped_frames[i] = tf(cropped_frames[i])
+        return cropped_frames
+
